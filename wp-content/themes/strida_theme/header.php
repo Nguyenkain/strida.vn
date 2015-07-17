56ac305def7a3eaaf7dafa6ffed9b6322635059d
Nguyenkain <Nguyenkain@gmail.com>
2015-07-17 11:30:55 +0700

    Complete homepage

--- wp-content/themes/strida_theme/header.php
+++ wp-content/themes/strida_theme/header.php
@@ -39,55 +39,6 @@
 	<script type="text/javascript"
 	        src="<?php bloginfo( 'template_url' ) ?>/modules/mod_cassrina_hover_image_menu/script_cassrina.js"
 	        tppabs="http://strida.vn//modules/mod_cassrina_hover_image_menu/script_cassrina.js"></script>
-	<script type="text/javascript">img1 = new Image();
-		img1.src = "<?php bloginfo( 'template_url' ) ?>/images/stories/navi_01.jpg"/*tpa=http://strida.vn/images/stories/navi_01.jpg*/;
-		img_roll1 = new Image();
-		img_roll1.src = "<?php bloginfo( 'template_url' ) ?>/images/stories/navi_01_o.jpg"/*tpa=http://strida.vn/images/stories/navi_01_o.jpg*/;
-		img2 = new Image();
-		img2.src = "<?php bloginfo( 'template_url' ) ?>/images/stories/navi_02.jpg"/*tpa=http://strida.vn/images/stories/navi_02.jpg*/;
-		img_roll2 = new Image();
-		img_roll2.src = "<?php bloginfo( 'template_url' ) ?>/images/stories/navi_02_o.jpg"/*tpa=http://strida.vn/images/stories/navi_02_o.jpg*/;
-		img3 = new Image();
-		img3.src = "<?php bloginfo( 'template_url' ) ?>/images/stories/navi_03.jpg"/*tpa=http://strida.vn/images/stories/navi_03.jpg*/;
-		img_roll3 = new Image();
-		img_roll3.src = "<?php bloginfo( 'template_url' ) ?>/images/stories/navi_03_o.jpg"/*tpa=http://strida.vn/images/stories/navi_03_o.jpg*/;
-		img4 = new Image();
-		img4.src = "<?php bloginfo( 'template_url' ) ?>/images/stories/navi_04.jpg"/*tpa=http://strida.vn/images/stories/navi_04.jpg*/;
-		img_roll4 = new Image();
-		img_roll4.src = "<?php bloginfo( 'template_url' ) ?>/images/stories/navi_04_o.jpg"/*tpa=http://strida.vn/images/stories/navi_04_o.jpg*/;
-		img5 = new Image();
-		img5.src = "<?php bloginfo( 'template_url' ) ?>/images/stories/navi_05.jpg"/*tpa=http://strida.vn/images/stories/navi_05.jpg*/;
-		img_roll5 = new Image();
-		img_roll5.src = "<?php bloginfo( 'template_url' ) ?>/images/stories/navi_05_o.jpg"/*tpa=http://strida.vn/images/stories/navi_05_o.jpg*/;
-		img6 = new Image();
-		img6.src = "<?php bloginfo( 'template_url' ) ?>/images/stories/navi_06.jpg"/*tpa=http://strida.vn/images/stories/navi_06.jpg*/;
-		img_roll6 = new Image();
-		img_roll6.src = "<?php bloginfo( 'template_url' ) ?>/images/stories/navi_06_o.jpg"/*tpa=http://strida.vn/images/stories/navi_06_o.jpg*/;
-		img7 = new Image();
-		img7.src = "<?php bloginfo( 'template_url' ) ?>/images/stories/navi_08.jpg"/*tpa=http://strida.vn/images/stories/navi_08.jpg*/;
-		img_roll7 = new Image();
-		img_roll7.src = "<?php bloginfo( 'template_url' ) ?>/images/stories/navi_08_o.jpg"/*tpa=http://strida.vn/images/stories/navi_08_o.jpg*/;
-		img8 = new Image();
-		img8.src = "<?php bloginfo( 'template_url' ) ?>/images/stories/navi_09_vn.jpg"/*tpa=http://strida.vn/images/stories/navi_09_vn.jpg*/;
-		img_roll8 = new Image();
-		img_roll8.src = "<?php bloginfo( 'template_url' ) ?>/images/stories/navi_09_vn_o.jpg"/*tpa=http://strida.vn/images/stories/navi_09_vn_o.jpg*/;
-		img9 = new Image();
-		img9.src = "<?php bloginfo( 'template_url' ) ?>/images/stories/navi_10.jpg"/*tpa=http://strida.vn/images/stories/navi_10.jpg*/;
-		img_roll9 = new Image();
-		img_roll9.src = "<?php bloginfo( 'template_url' ) ?>/images/stories/navi_10_o.jpg"/*tpa=http://strida.vn/images/stories/navi_10_o.jpg*/;
-		img10 = new Image();
-		img10.src = "<?php bloginfo( 'template_url' ) ?>/images/stories/navi_11.jpg"/*tpa=http://strida.vn/images/stories/navi_11.jpg*/;
-		img_roll10 = new Image();
-		img_roll10.src = "<?php bloginfo( 'template_url' ) ?>/images/stories/navi_11_o.jpg"/*tpa=http://strida.vn/images/stories/navi_11_o.jpg*/;
-		img53 = new Image();
-		img53.src = "<?php bloginfo( 'template_url' ) ?>/images/stories/navi_09.jpg"/*tpa=http://strida.vn/images/stories/navi_09.jpg*/;
-		img_roll53 = new Image();
-		img_roll53.src = "<?php bloginfo( 'template_url' ) ?>/images/stories/navi_09_o.jpg"/*tpa=http://strida.vn/images/stories/navi_09_o.jpg*/;
-		img55 = new Image();
-		img55.src = "<?php bloginfo( 'template_url' ) ?>/images/stories/sale_order.jpg"/*tpa=http://strida.vn/images/stories/sale_order.jpg*/;
-		img_roll55 = new Image();
-		img_roll55.src = "<?php bloginfo( 'template_url' ) ?>/images/stories/sale_order_o.jpg"/*tpa=http://strida.vn/images/stories/sale_order_o.jpg*/;
-	</script>
 
 	<link rel="stylesheet" href="<?php bloginfo( 'template_url' ) ?>/templates/stridatemplate/css/template.css"
 	      tppabs="http://strida.vn/templates/stridatemplate/css/template.css" type="text/css"/>
