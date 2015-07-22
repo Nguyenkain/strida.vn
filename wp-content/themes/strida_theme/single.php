e92aca72f783c152815c14e2f9eab66ba2c6d52c
Nguyenkain <Nguyenkain@gmail.com>
2015-07-22 17:54:30 +0700

    Fix bottom css

--- wp-content/themes/strida_theme/single.php
+++ wp-content/themes/strida_theme/single.php
@@ -17,7 +17,8 @@ get_header(); ?>
 					<?php if ( ICL_LANGUAGE_CODE == 'en' ) { ?>
 						<img src="<?php bloginfo( 'template_url' ) ?>/images/banners/news_list_title.jpg" alt="Banner">
 					<?php } else { ?>
-						<img src="<?php bloginfo( 'template_url' ) ?>/images/banners/news_list_title_vn.jpg" alt="Banner">
+						<img src="<?php bloginfo( 'template_url' ) ?>/images/banners/news_list_title_vn.jpg"
+						     alt="Banner">
 					<?php } ?>
 
 					<div class="clr"></div>
@@ -44,6 +45,20 @@ get_header(); ?>
 	<div id="page_content_right">
 		<div id="product_index" style="width: 95%; text-align: right;">
 		</div>
+
+		<table class="contentpaneopen">
+			<tbody>
+			<tr>
+				<td class="contentheading" width="100%">
+					<?php the_title() ?>
+				</td>
+			</tr>
+			<tr>
+				<td valign="top" class="createdate">
+					<?php echo date( "Y-m-d H:i:s", strtotime( get_the_date() ) ) ?>    </td>
+			</tr>
+			</tbody>
+		</table>
 		<?php the_content() ?>
 		<div style="clear:both"></div>
 	</div>
